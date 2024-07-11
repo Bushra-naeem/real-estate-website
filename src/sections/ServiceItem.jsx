33431aa88c62744f9beb4dd2af5379efaeea247f
Bushra <122994098+Bushra-naeem@users.noreply.github.com>
2024-07-12 02:04:16 +0500

    Update ServiceItem.jsx

--- src/sections/ServiceItem.jsx
+++ src/sections/ServiceItem.jsx
@@ -2,7 +2,7 @@ import { service_list } from "../assets/assets";
 const ServiceItem = ({image,heading,description,text}) => {
     {console.log(service_list);}
     return (
-        <div className="w-full flex flex-col items-center bg-white gap-6 px-12 py-16 my-5 shadow-xl">
+        <div className="w-full flex flex-col items-center bg-white gap-6 px-10 py-12 my-5 shadow-xl">
             <img src={image} alt="image" />
             <p className="text-xl sm:text-3xl font-bold">{heading}</p>
             <p className="text-center text-gray-500 mt-2 text-xl">{description}</p>
